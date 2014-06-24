bb0ad150edf0e2f2aafaf57d8c3210034074654d
Ritchie Cai <ritchiecai@r2labs.org>
2014-06-23 19:22:10 -0500

    project added jzy3d

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clj-plot3d "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "3D plotting library using Jzy3d"
+  :url "https://github.com/malloc82/clj-plot3d"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :repositories [["jzy3d-snapshots" "http://www.jzy3d.org/maven/snapshots"]
+                 ["jzy3d-releases"  "http://www.jzy3d.org/maven/releases"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.jzy3d/jzy3d-api "0.9.1"]])
